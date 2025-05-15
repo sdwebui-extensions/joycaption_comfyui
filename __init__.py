087b7ac61f77e101d9384af2a1310453a1851ac5
宣源 <xuanyuan.lb@alibaba-inc.com>
2025-05-15 09:17:23 +0800

    add model load

--- __init__.py
+++ __init__.py
@@ -3,8 +3,10 @@ from . import nodes
 NODE_CLASS_MAPPINGS = {
 	"JJC_JoyCaption": nodes.JoyCaption,
 	"JJC_JoyCaption_Custom": nodes.JoyCaptionCustom,
+    "JJC_JoyCaptionModelLoad": nodes.JoyCaptionModelLoad,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
-	"JJC_JoyCaption": "JoyCaption",
-	"JJC_JoyCaption_Custom": "JoyCaption (Custom)",
+	"JJC_JoyCaption": "JJC JoyCaption",
+	"JJC_JoyCaption_Custom": "JJC JoyCaption (Custom)",
+    "JJC_JoyCaptionModelLoad": "JJC JoyCaption Model Load"
 }
